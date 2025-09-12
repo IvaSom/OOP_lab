2af05f3d6aefdbc54d017ea2b6e2a61bbe6b4a1f
Ler0nBar0n <kotixxx6@gmail.com>
2025-09-12 12:43:21 +0400

    Добавил свою 1 лабу

---
+++ Laba 1 M/task2/MyFirstClass.java
@@ -0,0 +1,9 @@
+public class MyFirstClass {
+    public static void main(String[] s) {
+        s = new String[] {"Hello", "World", "!!!"};
+
+        for (String arr : s) {
+ 	        System.out.println(arr);
+        }
+    }
+}
---
+++ Laba 1 M/task4/MyFirstClass.java
@@ -0,0 +1,8 @@
+import myfirstpackage.*;
+
+public class MyFirstClass {
+    public static void main(String[] s) {
+            MySecondClass temp = new MySecondClass(5, 6);
+            System.out.println(temp.XOR());
+    }
+}
