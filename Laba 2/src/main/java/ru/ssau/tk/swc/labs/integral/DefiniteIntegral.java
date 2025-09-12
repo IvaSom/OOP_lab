c6079ece7371a69ea282439567113a0560ba6cf5
Ler0nBar0n <kotixxx6@gmail.com>
2025-09-12 16:48:22 +0400

    Добавил пакет и класс ОИ. Мне так Никита сказал сделать т.к. ОИ - не функции. Реализовал методы подсчета прямугл., трапеции и Симпсона. Попробую добавить тесты.

---
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/integral/DefiniteIntegral.java
@@ -0,0 +1,44 @@
+package ru.ssau.tk.swc.labs.integral;
+
+public class DefiniteIntegral {
+    private MathFunction function;
+
+    public DefiniteIntegral (MathFunction f) {this.function = f;}
+
+    public double methodTrapezoid (double a, double b, int n) {
+        if (a == b) return 0;
+
+        double h = (b - a) / n;
+        double sum = (function.apply(a) + function.apply(b)) / 2;
+
+        for (int i = 1; i < n; i++)
+            sum += function.apply(a + i * h;);
+
+        return sum * h;
+    }
+
+    public double methodRectangle (double a, double b, int n) {
+        if (a == b) return 0;
+
+        double h = (b - a) / n;
+        double sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += function.apply(a + i * h + h/2);
+
+        return sum * h;
+    }
+
+    public double methodSimpson (double a, double b, int n) {
+        if (a == b) return 0;
+        if (n % 2 == 1) n++;
+
+        double h = (b - a) / n;
+        double sum = function.apply(a) + function.apply(b);
+        for (int i = 1; i < n; i++) {
+            double coeff = (i % 2 == 0) ? 2 : 4;
+            sum += coeff * f.apply(a + i * h);
+        }
+        return sum * h / 3;
+    }
+
+}
