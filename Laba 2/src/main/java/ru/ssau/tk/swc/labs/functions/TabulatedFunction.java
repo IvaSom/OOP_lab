d80b186850482506e99a7f55cc845ce97be1c752
IvaSom <Iva_Som@mail.ru>
2025-09-13 15:51:56 +0400

    Добавил интерфейс TabulatedFunction

---
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/TabulatedFunction.java
@@ -0,0 +1,15 @@
+package ru.ssau.tk.swc.labs.functions;
+
+public interface TabulatedFunction {
+    int getCount(); //Метод получения количества табулированных значений
+    double getX(int index); //Метод, получающий значение аргумента x по номеру индекса
+    double getY(int index);//Метод, получающий значение y по номеру индекса
+    void setY(int index, double value); //Метод, задающий значение y по номеру индекса
+
+    int indexOfX(double x); //Метод, возвращающий индекс аргумента x, если нет то -1
+    int indexOfY(double y); //для y
+
+    double leftBound(); //самый левый x
+    double rightBound(); //самый правый x
+
+}
