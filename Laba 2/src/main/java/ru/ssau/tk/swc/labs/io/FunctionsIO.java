b6e9b284f4aab814f9773488358b7a8c336651d6
Ler0nBar0n <kotixxx6@gmail.com>
2025-10-05 23:48:20 +0400

    Сделал 1 часть 3 задания

---
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/io/FunctionsIO.java
@@ -0,0 +1,20 @@
+package ru.ssau.tk.swc.labs.io;
+
+import ru.ssau.tk.swc.labs.functions.*;
+import java.io.*;
+
+final public class FunctionsIO {
+    private FunctionsIO(){
+        throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+
+        for (Point p : function)
+           printWriter.printf("%f %f\n", p.x, p.y);
+
+        printWriter.flush();
+    }
+}
---
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/io/TabulatedFunctionFileWriter.java
@@ -0,0 +1,19 @@
+package ru.ssau.tk.swc.labs.io;
+
+import ru.ssau.tk.swc.labs.functions.*;
+import java.io.*;
+
+public class TabulatedFunctionFileWriter {
+    public static void main(String[] args) {
+        try(BufferedWriter arrayWriter = new BufferedWriter(new FileWriter("output/array function.txt"));
+            BufferedWriter listWriter = new BufferedWriter(new FileWriter("output/linked list function.txt"));) {
+            TabulatedFunction arrayFunction = new ArrayTabulatedFunction(new double[]{37, 42, 73}, new double[]{3, 22, 14});
+            TabulatedFunction listFunction = new LinkedListTabulatedFunction(new CosFunction(), 0, 1, 45);
+
+            FunctionsIO.writeTabulatedFunction(arrayWriter, arrayFunction);
+            FunctionsIO.writeTabulatedFunction(listWriter, listFunction);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
