1ca4438df785a2e39063f74de84aab00010f7e21
IvaSom <Iva_Som@mail.ru>
2025-10-10 14:36:31 +0400

    Добавил WriteTask

---
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/concurrent/WriteTask.java
@@ -0,0 +1,21 @@
+package ru.ssau.tk.swc.labs.concurrent;
+
+
+import ru.ssau.tk.swc.labs.functions.*;
+import  java.lang.Runnable;
+
+public class WriteTask implements Runnable{
+    private TabulatedFunction function;
+    double value;
+    public WriteTask(TabulatedFunction function, double value){
+        this.function=function;
+        this.value=value;
+    }
+    @Override
+    public void run(){
+        for (int i=0; i<function.getCount(); i++){
+            function.setY(i, value);
+            System.out.printf("Writing for index %d complete", i);
+        }
+    }
+}
