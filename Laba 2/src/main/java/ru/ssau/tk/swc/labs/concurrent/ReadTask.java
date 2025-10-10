a770a8964fe62fde11942f22b82d257d95a5430b
IvaSom <Iva_Som@mail.ru>
2025-10-10 14:25:07 +0400

    Добавил ReadTask

---
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/concurrent/ReadTask.java
@@ -0,0 +1,19 @@
+package ru.ssau.tk.swc.labs.concurrent;
+
+import ru.ssau.tk.swc.labs.functions.*;
+
+import  java.lang.Runnable;
+
+public class ReadTask implements Runnable{
+    private TabulatedFunction function;
+    public ReadTask(TabulatedFunction function){
+        this.function=function;
+    }
+    @Override
+    public void run(){
+        for (int i=0; i<function.getCount(); i++){
+            System.out.printf("After read: i = %d, x = %f, y = %f\n",i, function.getX(i), function.getY(i));
+            //%d для int, %f для float и double, %s для строк, можно %n вместо \n это одинаково
+        }
+    }
+}
