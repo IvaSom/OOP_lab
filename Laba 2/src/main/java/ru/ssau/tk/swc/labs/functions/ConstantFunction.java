cce4cd0047b66255405753e9ee26be6155093ebf
Ler0nBar0n <kotixxx6@gmail.com>
2025-11-04 03:15:53 +0400

    Добавил еще пару логов

--- Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/ConstantFunction.java
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/ConstantFunction.java
@@ -1,6 +1,10 @@
 package ru.ssau.tk.swc.labs.functions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ConstantFunction implements MathFunction{
+    private static final Logger logger = LoggerFactory.getLogger(ConstantFunction.class);
     private final double constant;
 
     public ConstantFunction (double con) {this.constant = con;}
@@ -11,6 +15,7 @@ public class ConstantFunction implements MathFunction{
 
     @Override
     public double apply(double x) {
+        logger.info("Установлена константа");
         return constant;
     }
 }
