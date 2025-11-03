1342fb5b5f014b21769aa773384bd9de201f9a7d
Ler0nBar0n <kotixxx6@gmail.com>
2025-11-04 03:15:53 +0400

    Добавил еще пару логов

--- Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/CompositeFunction.java
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/CompositeFunction.java
@@ -1,6 +1,10 @@
 package ru.ssau.tk.swc.labs.functions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CompositeFunction implements MathFunction {
+    private static final Logger logger = LoggerFactory.getLogger(CompositeFunction.class);
     private final MathFunction fun1;
     private final MathFunction fun2;
     public CompositeFunction(MathFunction fun1, MathFunction fun2) {
@@ -9,6 +13,7 @@ public class CompositeFunction implements MathFunction {
     }
     @Override
     public double apply(double x) {
+        logger.info("Выполнена вложенная функция");
         return fun2.apply(fun1.apply(x));
     }
 
--- Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/CosFunction.java
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/CosFunction.java
@@ -1,6 +1,13 @@
 package ru.ssau.tk.swc.labs.functions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CosFunction implements MathFunction{
+    private static final Logger logger = LoggerFactory.getLogger(CosFunction.class);
     @Override
-    public double apply(double x) {return Math.cos(x);}
+    public double apply(double x) {
+        logger.info("Выполнена функция косинуса");
+        return Math.cos(x);
+    }
 }
