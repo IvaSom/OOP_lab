644fd15dced9ed9c6f31d02396194c4f0748957d
IvaSom <Iva_Som@mail.ru>
2025-11-04 19:15:46 +0400

    Добавил логи в пакет functions

--- Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/SqrFunction.java
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/SqrFunction.java
@@ -1,8 +1,13 @@
 package ru.ssau.tk.swc.labs.functions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SqrFunction implements MathFunction {
+    private static final Logger logger = LoggerFactory.getLogger(SqrFunction.class);
     @Override
     public double apply (double x) {
+        logger.info("Выполнена функция квадрата");
         return x * x;
     }
 }
--- Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/UnitFunction.java
+++ Laba 2/src/main/java/ru/ssau/tk/swc/labs/functions/UnitFunction.java
@@ -1,6 +1,13 @@
 package ru.ssau.tk.swc.labs.functions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class UnitFunction implements MathFunction{
+    private static final Logger logger = LoggerFactory.getLogger(UnitFunction.class);
     @Override
-    public double apply (double x) {return 1;}
+    public double apply (double x) {
+        logger.info("Выполнена функция y=1");
+        return 1;
+    }
 }
